40c1339b32a81fd520b6b87d1d8b3d7584ca297a
ahmadullah <nikzadahmadullah@gmail.com>
2025-12-14 22:50:41 +0100

    database_url maybe solved

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from '../app/generated/prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { Pool } from '@neondatabase/serverless'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const connectionString = process.env.DATABASE_URL!
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool as any)
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+})
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
     adapter,
